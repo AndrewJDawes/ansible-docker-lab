a0e8d064622025518bce5cb54096e5f32c47e6a5
Andrew Dawes <andrewjamesdawes@gmail.com>
2023-10-01 08:03:43 -0400

    Set up sudo user with NOPASSWD

--- ansible/example.sh
+++ ansible/example.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-ansible ubuntu-1 -m ping
+# No root needed
+# ansible ubuntu-1 -m ping
+# Become root and ask for sudo password
+# ansible all -b -K -m ping
+# Become root and do not ask for password - assumes user has NOPASSWD in remote host's /etc/sudoers
+ansible all -b -m ping
